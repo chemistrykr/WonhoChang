6fffc1b924f96d73e98660dcda6e769b466d5522
WONHO CHANG <chemistrykr1@gmail.com>
2025-04-03 00:33:59 +0900

    upd

---
+++ my-tina-blog/tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: 'C:/Users/user/WonhoChang/my-tina-blog/tina/__generated__/.cache/1743607718467', url: 'https://content.tinajs.io/1.5/content/ca101170-a52f-4cb8-9bf2-dd951d0f98d0/github/main', token: '9a933c2a1a33df4fb33fb8d1e60dcedd96cc1a1d', queries,  });
+export default client;
+  
