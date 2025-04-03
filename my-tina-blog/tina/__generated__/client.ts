5f1c13915f579c26dea4a16557177cd05246af6e
WONHO CHANG <chemistrykr1@gmail.com>
2025-04-03 20:41:40 +0900

    push tina

--- my-tina-blog/tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '9a933c2a1a33df4fb33fb8d1e60dcedd96cc1a1d', queries,  });
-export default client;
-  
